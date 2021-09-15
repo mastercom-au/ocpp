68edd6eeb55346dbf3a6245fda915ebc5396dc52
Lewis Hyman <lewis.hyman@mastercom.com.au>
2021-09-15 15:08:28 +1000

    updated definitions, fixed library definition

--- src/schema.rs
+++ src/schema.rs
@@ -3,7 +3,51 @@ extern crate serde;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "camelCase")]
 pub struct Authorize{
-    idTag: String,
+    pub id_tag: String,
 }
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+    pub struct BootNotification{
+    pub    charge_point_vendor:             String,
+    pub    charge_point_model:              String,
+    pub    charge_point_serial_number:      String,
+    pub    charge_box_serial_number:        String,
+    pub    firmware_version:                String,
+    pub    iccid:                           String,
+    pub    imsi:                            String,
+    pub    meter_type:                      String,
+    pub    meter_serial_number:             String,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+pub struct ChangeAvailability{
+    pub connector_id: String,
+}
+
+/*#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "snake_case")]*/
