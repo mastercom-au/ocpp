7738c0eb8527748306b988dbb6da1c6b30c2e85f
Lewis Hyman <lewis.hyman@mastercom.com.au>
2021-09-22 09:40:50 +1000

    updated structs, still missing enum definitions and complicated schema

--- src/schema.rs
+++ src/schema.rs
@@ -3,116 +3,125 @@ extern crate serde;
 extern crate jsonschema;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Authorize{
-    pub id_tag:                         String,
+    pub id_tag: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct BootNotification{
     pub charge_point_vendor: String,
     pub charge_point_model: String,
-    pub charge_point_serial_number: String,
-    pub charge_box_serial_number: String,
-    pub firmware_version: String,
-    pub iccid: String,
-    pub imsi: String,
-    pub meter_type: String,
-    pub meter_serial_number: String,
+    pub charge_point_serial_number: Option<String>,
+    pub charge_box_serial_number: Option<String>,
+    pub firmware_version: Option<String>,
+    pub iccid: Option<String>,
+    pub imsi: Option<String>,
+    pub meter_type: Option<String>,
+    pub meter_serial_number: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ChangeAvailability{
     pub connector_id: u32,
-    pub type: enum type{"Inoperative","operative"}
+    pub r#type: String,                                   //ENUM "Inoperative", "Operative"
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ChangeConfiguration{
     pub key: String,
     pub value: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ClearCache{
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct DataTransfer{
     pub vendor_id: String,
-    pub message_id: String,
-    pub data: String
+    pub message_id: Option<String>,
+    pub data: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct GetConfiguration{
     pub key: Vec<String>,
 }
 
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Heartbeat{
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct MeterValues{
-    //RETURN TO ME 
+    pub connector_id: u32,
+    pub transaction_id: u32,
+    pub meter_value: Vec<String>,                   //complex
+
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct RemoteStartTransaction{
-    //RETURN TO ME 
+                                                    //complex
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct RemoteStopTransaction{
-    //RETURN TO ME 
+    pub transaction_id: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Reset{
-    //RETURN TO ME 
+                                                    //complex
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct StartTransaction{
-    pub connector_id: 
+    pub connector_id: u32,
+    pub id_tag: String,
+    pub meter_start: i32,
+    pub reservation_id: Option<i32>,
+    pub timestamp: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-pub struct StopTransaction{
 
-}
-
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct StatusNotification{
-
+    pub connector_id: u32,
+    error_code: String,
+    info: Option<String>,
+    status: String,
+    timestamp: Option<String>,
+    vendor_id: Option<String>,
+    vendor_error_code: Option<String>
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct StopTransaction{
+                                                    //complex
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct UnlockConnector{
-
+    pub connector_id: u32,
 }
-
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]*/
