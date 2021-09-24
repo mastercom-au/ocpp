3d8efe9a27faa7fb083eb78dcc2e9cf4b86a3436
Lewis Hyman <lewis.hyman@mastercom.com.au>
2021-09-24 15:44:07 +1000

    Formatting fixes, added metervalue

--- src/schema.rs
+++ src/schema.rs
@@ -1,20 +1,20 @@
-#[macro_use]
-use serde;
+//#[macro_use]
 use chrono::DateTime;
 use chrono::Utc;
-use jsonschema;
-use serde::{Serialize, Deserialize};
-use serde_json;
+use serde;
+//use jsonschema;
+use serde::{Deserialize, Serialize};
+//use serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct Authorize{
+pub struct Authorize {
     pub id_tag: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct BootNotification{
+pub struct BootNotification {
     pub charge_point_vendor: String,
     pub charge_point_model: String,
     pub charge_point_serial_number: Option<String>,
@@ -28,27 +28,31 @@ pub struct BootNotification{
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct ChangeAvailability{
+pub struct ChangeAvailability {
     pub connector_id: u32,
-    pub r#type: String,                                   //ENUM "Inoperative", "Operative" 
+    pub r#type: ChangeAvailabilityType,
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum ChangeAvailabilityType {
+    Inoperative,
+    Operative,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct ChangeConfiguration{
+pub struct ChangeConfiguration {
     pub key: String,
     pub value: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct ClearCache{
-}
+pub struct ClearCache {}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct DataTransfer{
+pub struct DataTransfer {
     pub vendor_id: String,
     pub message_id: Option<String>,
     pub data: Option<String>,
@@ -56,46 +60,189 @@ pub struct DataTransfer{
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct GetConfiguration{
+pub struct GetConfiguration {
     pub key: Vec<String>,
 }
 
-
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct Heartbeat{
-}
+pub struct Heartbeat {}
 
+//---------------------------------------------METER VALUE-----------------
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct MeterValues{
+pub struct MeterValues {
     pub connector_id: u32,
     pub transaction_id: u32,
-    pub meter_value: Vec<String>,                   //complex
-
+    pub meter_value: Vec<MVValues>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct RemoteStartTransaction{
-                                                    //complex
+pub struct MVValues {
+    pub timestamp: DateTime<Utc>,
+    pub sampled_value: Vec<MVSampledValue>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct RemoteStopTransaction{
+pub struct MVSampledValue {
+    pub value: String,
+    pub context: MVSampledContext,
+    pub format: MVSampledFormat,
+    pub measurant: MVSampledMeasurand,
+    pub phase: MVSampledPhase,
+    pub location: MVSampledLocation,
+    pub unit: MVSampledUnit,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum MVSampledContext {
+    #[serde(rename = "Interruption.Begin")]
+    InterruptionBegin,
+    #[serde(rename = "Interruption.End")]
+    InterruptionEnd,
+    #[serde(rename = "Sample.Clock")]
+    SampleClock,
+    #[serde(rename = "Sample.Periodic")]
+    SamplePeriodic,
+    #[serde(rename = "Transaction.Begin")]
+    TransactionBegin,
+    #[serde(rename = "Transaction.End")]
+    TransactionEnd,
+    Trigger,
+    Other,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum MVSampledFormat {
+    Raw,
+    SignedData,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum MVSampledMeasurand {
+    #[serde(rename = "Energy.Active.Export.Register")]
+    EnergyActiveExportRegister,
+    #[serde(rename = "Energy.Active.Import.Register")]
+    EnergyActiveImportRegister,
+    #[serde(rename = "Energy.Reactive.Export.Register")]
+    EnergyReactiveExportRegister,
+    #[serde(rename = "Energy.Reactive.Import.Register")]
+    EnergyReactiveImportRegister,
+    #[serde(rename = "Energy.Active.Export.Interval")]
+    EnergyActiveExportInterval,
+    #[serde(rename = "Energy.Active.Import.Interval")]
+    EnergyActiveImportInterval,
+    #[serde(rename = "Energy.Reactive.Export.Interval")]
+    EnergyReactiveExportInterval,
+    #[serde(rename = "Energy.Reactive.Import.Interval")]
+    EnergyReactiveImportInterval,
+    #[serde(rename = "Power.Active.Export")]
+    PowerActiveExport,
+    #[serde(rename = "Power.Active.Import")]
+    PowerActiveImport,
+    #[serde(rename = "Power.Offered")]
+    PowerOffered,
+    #[serde(rename = "Power.Reactive.Export")]
+    PowerReactiveExport,
+    #[serde(rename = "Power.Reactive.Import")]
+    PowerReactiveImport,
+    #[serde(rename = "Power.Factor")]
+    PowerFactor,
+    #[serde(rename = "Current.Import")]
+    CurrentImport,
+    #[serde(rename = "Current.Export")]
+    CurrentExport,
+    #[serde(rename = "Current.Offered")]
+    CurrentOffered,
+    Voltage,
+    Frequency,
+    Temperature,
+    SoC,
+    RPM,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum MVSampledPhase {
+    L1,
+    L2,
+    L3,
+    N,
+    #[serde(rename = "L1-N")]
+    L1N,
+    #[serde(rename = "L2-N")]
+    L2N,
+    #[serde(rename = "L3-N")]
+    L3N,
+    #[serde(rename = "L1-L2")]
+    L1L2,
+    #[serde(rename = "L2-L3")]
+    L2L3,
+    #[serde(rename = "L3-L1")]
+    L3L1,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum MVSampledLocation {
+    Cable,
+    EV,
+    Inlet,
+    Outlet,
+    Body,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[warn(non_camel_case_types)]
+pub enum MVSampledUnit {
+    #[serde(rename = "Wh")]
+    WH,
+    #[serde(rename = "kWh")]
+    KWh,
+    #[serde(rename = "varh")]
+    Varh,
+    #[serde(rename = "kvarh")]
+    KVarh,
+    #[serde(rename = "W")]
+    W,
+    #[serde(rename = "kW")]
+    KW,
+    VA,
+    KVA,
+    #[serde(rename = "var")]
+    Var,
+    #[serde(rename = "kvar")]
+    KVar,
+    A,
+    V,
+    K,
+    Celsius,
+    Fahrenheit,
+    Percent,
+}
+//-----------------------------------------END METER VALUE-----------------
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct RemoteStartTransaction {
+    //complex
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct RemoteStopTransaction {
     pub transaction_id: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct Reset{
-                                                    //complex
+pub struct Reset {
+    //complex
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct StartTransaction{
+pub struct StartTransaction {
     pub connector_id: u32,
     pub id_tag: String,
     pub meter_start: i32,
@@ -103,27 +250,26 @@ pub struct StartTransaction{
     pub timestamp: String,
 }
 
-
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct StatusNotification{
+pub struct StatusNotification {
     pub connector_id: u32,
     error_code: String,
     info: Option<String>,
     status: String,
     timestamp: Option<String>,
     vendor_id: Option<String>,
-    vendor_error_code: Option<String>
+    vendor_error_code: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct StopTransaction{
-                                                    //complex
+pub struct StopTransaction {
+    //complex
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct UnlockConnector{
+pub struct UnlockConnector {
     pub connector_id: u32,
 }
--- src/test.rs
+++ src/test.rs
@@ -1,17 +1,18 @@
-use mp70_interface::schema as json;
+use crate::schema;
 
-fn main (){
+fn main() {
     println!("Compiles!");
-    let test = json::BootNotification {charge_point_vendor:"test1".to_string(),
-                                            charge_point_model:"test2".to_string(),
-                                            charge_point_serial_number:"test3".to_string(),
-                                            charge_box_serial_number:"test4".to_string(),
-                                            firmware_version:"test5".to_string(),
-                                            iccid:"test6".to_string(),
-                                            imsi:"test7".to_string(),
-                                            meter_type:"test8".to_string(),
-                                            meter_serial_number:"test9".to_string(),
-                                            };
+    let test = schema::BootNotification {
+        charge_point_vendor: "test1".to_string(),
+        charge_point_model: "test2".to_string(),
+        charge_point_serial_number: Some("test3".to_string()),
+        charge_box_serial_number: Some("test4".to_string()),
+        firmware_version: Some("test5".to_string()),
+        iccid: Some("test6".to_string()),
+        imsi: Some("test7".to_string()),
+        meter_type: Some("test8".to_string()),
+        meter_serial_number: Some("test9".to_string()),
+    };
 
     println!("{:?}", test);
 }
