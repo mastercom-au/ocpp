c81abc96379d4a7ba4002212a51ed40e5fc9bb98
Lewis Hyman <lewis.hyman@mastercom.com.au>
2021-11-11 08:51:38 +1100

    Testing merge behaviour

--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,9 @@
 use crate::core::{self, BootNotificationRequest, BootNotificationResponse};
-use ocpp_json_validate::JsonValidate;
 use chrono::Utc;
+use ocpp_json_validate::JsonValidate;
 
 #[test]
-fn test_boot_notification_response_validates()
-{
+fn test_boot_notification_response_validates() {
     let bn_res = BootNotificationResponse {
         current_time: Utc::now(),
         status: core::BootNotificationStatus::Accepted,
@@ -14,10 +13,9 @@ fn test_boot_notification_response_validates()
     assert!(bn_res.validate().is_ok());
 }
 
-
+//ARBITRARY LINE OF CODE HERE TO TEST CRATE PUSH?
 #[test]
-fn test_boot_notification_request_validates()
-{
+fn test_boot_notification_request_validates() {
     let bn_req = BootNotificationRequest {
         charge_point_vendor: "test1".to_string(),
         charge_point_model: "test2".to_string(),
@@ -34,8 +32,7 @@ fn test_boot_notification_request_validates()
 }
 
 #[test]
-fn test_boot_notification_request_charge_point_model_string_length_limit()
-{
+fn test_boot_notification_request_charge_point_model_string_length_limit() {
     let bn_req = BootNotificationRequest {
         charge_point_vendor: "test1".to_string(),
         charge_point_model: "test2 AND SOME ARBITRARILY LONG STRING HERE TO BREAK THINGS"
