4030516eeabdfe7bbdebe4f5ebef7a79dad7fb9d
Lewis Hyman <lewis.hyman@mastercom.com.au>
2022-02-01 16:39:51 +1100

    documentation for boot notification

--- src/point_init/boot_notification.rs
+++ src/point_init/boot_notification.rs
@@ -4,6 +4,39 @@ use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 use strum_macros::Display;
 
+/// After start-up, a Charge Point SHALL send a request to the Central System with information about its
+/// configuration (e.g. version, vendor, etc.). The Central System SHALL respond to indicate whether it will accept the
+/// Charge Point.
+/// The Charge Point SHALL send a BootNotification.req PDU each time it boots or reboots. Between the physical
+/// power-on/reboot and the successful completion of a BootNotification, where Central System returns Accepted or
+/// Pending, the Charge Point SHALL NOT send any other request to the Central System. This includes cached
+/// messages that are still present in the Charge Point from before.
+///
+/// When the Central System responds with a BootNotification.conf with a status Accepted, the Charge Point will
+/// adjust the heartbeat interval in accordance with the interval from the response PDU and it is RECOMMENDED to
+/// synchronize its internal clock with the supplied Central System’s current time. If the Central System returns
+/// something other than Accepted, the value of the interval field indicates the minimum wait time before sending a
+/// next BootNotification request. If that interval value is zero, the Charge Point chooses a waiting interval on its
+/// own, in a way that avoids flooding the Central System with requests. A Charge Point SHOULD NOT send a
+/// BootNotification.req earlier, unless requested to do so with a TriggerMessage.req.
+///
+/// If the Central System returns the status Rejected, the Charge Point SHALL NOT send any OCPP message to the
+/// Central System until the aforementioned retry interval has expired. During this interval the Charge Point may no
+/// longer be reachable from the Central System. It MAY for instance close its communication channel or shut down
+/// its communication hardware. Also the Central System MAY close the communication channel, for instance to
+/// free up system resources. While Rejected, the Charge Point SHALL NOT respond to any Central System initiated
+/// message. the Central System SHOULD NOT initiate any.
+///
+/// The Central System MAY also return a Pending registration status to indicate that it wants to retrieve or set
+/// certain information on the Charge Point before the Central System will accept the Charge Point. If the Central
+/// System returns the Pending status, the communication channel SHOULD NOT be closed by either the Charge
+/// Point or the Central System. The Central System MAY send request messages to retrieve information from the
+/// Charge Point or change its configuration. The Charge Point SHOULD respond to these messages. The Charge
+/// Point SHALL NOT send request messages to the Central System unless it has been instructed by the Central
+/// System to do so with a TriggerMessage.req request.
+/// While in pending state, the following Central System initiated messages are not allowed:
+/// RemoteStartTransaction.req and RemoteStopTransaction.req
+
 // -------------------------- REQUEST --------------------------
 #[json_validate("../json_schemas/BootNotification.json")]
 #[skip_serializing_none]
@@ -13,11 +46,17 @@ pub struct BootNotificationRequest {
     pub charge_point_vendor: String,
     pub charge_point_model: String,
     pub charge_point_serial_number: Option<String>,
+    /// Identifies the serial number of the Charge Box inside the Charge Point. Deprecated, will be removed in future version.
     pub charge_box_serial_number: Option<String>,
+    /// Identifies the firmware version on the charge .
     pub firmware_version: Option<String>,
+    /// Identifies the ICCID of the modem's SIM card.
     pub iccid: Option<String>,
+    /// Identifies the IMSI of the modem's SIM card.
     pub imsi: Option<String>,
+    /// Identifies the type of the main electrical meter of the charge point.
     pub meter_type: Option<String>,
+    /// Identifies the serial numbver of the main electrical meter of the charge point.
     pub meter_serial_number: Option<String>,
 }
 
@@ -26,14 +65,19 @@ pub struct BootNotificationRequest {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct BootNotificationResponse {
+    /// Identifies whether the charge point has been registered with the central server.
     pub status: BootNotificationStatus,
     pub current_time: DateTime<Utc>,
+    /// When status is accepted, contains the heartbeat inverval in seconds. If status is not accepted, contains a timeout value before the charge point can retry bootnotifacion.
     pub interval: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Display)]
 pub enum BootNotificationStatus {
+    /// Chargepoint accepted by central system
     Accepted,
+    /// Acceptance pending. Central system may send messages to retrieve information or prepare the charge point.
     Pending,
+    // Charge point not accepted, i.e. chargepointID is not known
     Rejected,
 }
